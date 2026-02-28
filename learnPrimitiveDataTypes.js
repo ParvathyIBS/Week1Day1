491d2c5c9e21aa97fabad2caba01a071d1624f46
ParvathyIBS <93532845+ParvathyIBS@users.noreply.github.com>
2026-02-28 18:02:41 +0530

    Add files via upload

---
+++ learnPrimitiveDataTypes.js
@@ -0,0 +1,13 @@
+
+
+var firstName="Parvathy"
+var companyName="IBS"
+var mobileNumber=12345678
+var isAutomation=true
+var hasPlayWright=false
+
+console.log(typeof firstName)
+console.log(typeof companyName)
+console.log(typeof mobileNumber)
+console.log(typeof isAutomation)
+console.log(typeof hasPlayWright)
